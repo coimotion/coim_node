5be64ea9945a0e8ea5627f831438cec12cf5ae26
Ben Lue <ben.lue@gocharm.com.tw>
2014-07-17 18:06:02 +0800

    testing repo

--- test.js
+++ test.js
@@ -1,4 +1,25 @@
-var  PageEnty = require('./lib/entity/PageEnty');
+var  coim = require('./lib/coim'),
+     PageEnty = require('./lib/entity/PageEnty'),
+     Repo = require('./lib/entity/Repo');
 
+coim.init();
+
+coim.login('core/user/login', {accName: 'ben.lue@gocharm.com.tw', passwd: '000000'}, function(result) {
+    console.log('Login result: [%d] %s', result.errCode, result.message);
+},
+function(err) {
+    console.log('Oops! Failed to login.')
+});
+
+/*
+Repo.getRepo('ben_lue', function(err, myRepo) {
+    if (err)
+        console.log('Error as: %s', JSON.stringify(myRepo));
+    else
+        console.log('Yes! Got the repository.');
+});
+*/
+/*
 var  pgEnty = new PageEnty({}, 'articles', {title: 'My Article', ngID: 3});
 console.log('Name of the id is %s', pgEnty.getId());
+*/
